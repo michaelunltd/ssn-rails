0b1a45ce5cdf4bc24c96d163326e6649dd04355d
Michael Rasheed Tan <mikkoyunltd@gmail.com>
2017-07-22 17:25:21 +0800

    added helper methods for devise

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,20 @@ module ApplicationHelper
   def header(text)
     content_for(:header) { text.to_s }
   end
+
+	def resource_name
+		:user
+	end
+
+	def resource
+		@user ||= User.new
+	end
+
+	def devise_mapping
+		@devise_mapping ||= Devise.mappings[:user]
+	end
+
+	def resource_class
+		User
+	end
 end
