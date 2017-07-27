b441b440af2bb9fd459577f7562a37bab9c9aa3c
Michael Rasheed H. Tan <mikkoyunltd@gmail.com>
2017-07-27 22:30:59 +0800

    added css and modified home index

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :posts, :dependent => :destroy
-  
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :first_name, :last_name, presence: true
 
   def full_name
     "#{first_name} #{last_name}"
